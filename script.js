67fa48f4fbb42d8dce11c431e6a8b87cfd0fb6a8
Abdurrahman <beketabdurrahman1950@gmail.com>
2024-10-30 20:52:30 +0500

    day-3js-w-1

---
+++ script.js
@@ -0,0 +1,23 @@
+var name = "Abdurrahman"
+
+var name = "Galymbek"
+
+let a = 5;
+
+if(true){
+    let a=10;
+    console.log("a:" + a);
+    
+}
+console.log("a:"+ a);
+
+
+const text= "Сыздын поролныз немесе логиныныз кате"
+
+const PI = 3.14;
+
+// PI=2.15
+
+console.log("PI:" + PI);
+
+
